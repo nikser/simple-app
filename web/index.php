46d3c798f3d29259130139700f866f948707aad3
Nikolai Efimov <nefimov@plesk.com>
2019-04-19 17:24:46 +0700

    Initial commit

---
+++ web/index.php
@@ -0,0 +1,4 @@
+<h1 style="color: #5e9ca0; text-align: center;">Congratulations!<img src="img/confetti-small.png" width="40" /></h1>
+<h2 style="color: #2e6c80; text-align: center;">You are in Kubernetes!</h2>
+<p><img style="display: block; margin-left: auto; margin-right: auto;" src="img/kubernetes-logo.png" alt="html cleaner" width="350" /></p>
+<?php echo date("Y-m-d H:i:s"); ?>
